9baeb4454db5ebd5fb7f00d6c8f26751aa886c70
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-03-10 17:32:25 +0500

    all tests have passed

---
+++ Debugging Fundamentals/Task1/Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object comparedObject)
+        {
+            Product product = comparedObject as Product;
+
+            if (product is null)
+                return false;
+
+            if (product.Name == this.Name && product.Price == this.Price)
+                return true;
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Name, this.Price);
+        }
+    }
+}
