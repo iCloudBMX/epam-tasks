43cdd6500310bf2a99ca9875509da3667a39a20e
Khondamir Abdukhoshimov <iCloudBMX0525@gmail.com>
2022-04-05 11:02:43 +0500

    TF of task2 changed to net5.0

--- Serialization/task2/Program.cs
+++ Serialization/task2/Program.cs
@@ -1,10 +1,13 @@
-﻿using task2;
+﻿using System;
+using task2;
 
 Person person =
     new Person(name: "Khondamir", age: 20);
 
 BinarySerializer binarySerializer =
     new BinarySerializer(path: "person.bin");
+
+binarySerializer.Serialize(person);
 
 Person deserializedPerson =
     binarySerializer.Deserialize();
