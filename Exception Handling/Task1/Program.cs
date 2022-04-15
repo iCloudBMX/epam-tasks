74ff0f1c341983a3d08778f3d17937f8a36bdf33
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-04-15 07:26:18 +0500

    Exception Handling task

---
+++ Exception Handling/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            foreach(string value in input)
+            {
+                try
+                {
+                    Console.WriteLine(value[0]);
+                }
+                catch(Exception exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+            }
+        }
+    }
+}
