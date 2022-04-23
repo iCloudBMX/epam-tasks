9d460052d0335be0a582a2645637b88c371d104d
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-04-23 17:28:47 +0500

    ShouldFindMaxBinaryGap -> FAIL

---
+++ UnitTesting/Katas.Tests/BinaryGapTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Katas.Tests
+{
+    public class BinaryGapTests
+    {
+        [Theory]
+        [InlineData(9, 2)]
+        [InlineData(529, 4)]
+        [InlineData(1041, 5)]
+        public void ShouldFindMaxBinaryGap(int number, int expectedResult)
+        {
+            // Act
+            int actualResult =
+                BinaryGap.FindMaxBinaryGap(number);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
---
+++ UnitTesting/Katas/BinaryGap/BinaryGap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Katas
+{
+    public class BinaryGap
+    {
+        public static int FindMaxBinaryGap(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
