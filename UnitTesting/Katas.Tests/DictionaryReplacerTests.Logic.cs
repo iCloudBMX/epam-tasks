d9930ac11147e767a55a599b3560db2301b88d50
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-04-23 16:34:51 +0500

    ShouldReplaceSuffixWithKeys_WithMultipleSuffixes -> PASS

--- UnitTesting/Katas.Tests/DictionaryReplacerTests.Logic.cs
+++ UnitTesting/Katas.Tests/DictionaryReplacerTests.Logic.cs
@@ -26,5 +26,28 @@ namespace Katas.Tests
             // Assert
             Assert.Equal(expectedResult, actualResult);
         }
+
+        [Fact]
+        public void ShouldReplaceSuffixWithKeys_WithMultipleSuffixes()
+        {
+            // Arrange
+            string inputString = "$temp$ here comes the name $name$";
+            var inputDictionary = new Dictionary<string, string>
+            {
+                { "temp", "temporary" },
+                { "name", "John Doe" }
+            };
+
+            string expectedResult = "temporary here comes the name John Doe";
+
+            // Act
+            var actualResult =
+                DictionaryReplacer.ReplaceSuffixWithKeys(
+                    inputString: inputString,
+                    dictionary: inputDictionary);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
     }
 }
