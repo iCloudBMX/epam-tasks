9f45cfce4e421c9c8064427bc8758b2a31b6f51e
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-04-23 16:14:57 +0500

    ShouldReplaceSuffixWithKeys -> FAIL

---
+++ UnitTesting/Katas.Tests/DictionaryReplacerTests.Logic.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Katas.Tests
+{
+    public partial class DictionaryReplacerTests
+    {
+        [Fact]
+        public void ShouldReplaceSuffixWithKeys()
+        {
+            // Arrange
+            string inputString = "$temp$";
+            var inputDictionary = new Dictionary<string, string>
+            {
+                { "temp", "temporary" }
+            };
+
+            string expectedResult = "temporary";
+
+            // Act
+            var actualResult = 
+                DictionaryReplacer.ReplaceSuffixWithKeys(
+                    inputString: inputString,
+                    dictionary: inputDictionary);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
---
+++ UnitTesting/Katas/DictionaryReplacer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Katas
+{
+    public class DictionaryReplacer
+    {
+        public static string ReplaceSuffixWithKeys(
+            string inputString, 
+            Dictionary<string, string> dictionary)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
