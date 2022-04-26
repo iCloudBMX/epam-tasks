8cace225f76d80bc4b40783c751ef763b4632cc5
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-04-26 15:02:22 +0500

    Task2 Implemented

---
+++ Task1/SQLDB/Views/EmployeeInfo.sql
@@ -0,0 +1,16 @@
+﻿CREATE VIEW [dbo].[EmployeeInfo]
+AS 
+	SELECT TOP(100) PERCENT
+		[Employee].[Id] AS EmployeeId, 
+		CASE
+			WHEN [Employee].[EmployeeName] IS NULL THEN ([Person].[FirstName] + ' ' + [Person].[LastName])
+			ELSE [Employee].[EmployeeName]
+		 END AS EmployeeFullName,  
+		([Address].[ZipCode] + '_' + [Address].[State] + ',' + [Address].[City] + '-' + [Address].[Street]) as EmployeeFullAddress,
+		([Employee].[CompanyName] + '(' + [Employee].[Position] + ')') as EmployeeCompanyFullInfo
+		
+		FROM [Employee] 
+			JOIN [Person] ON [Person].[Id] = [Employee].[PersonId]
+			JOIN [Address] ON [Address].[Id] = [Employee].[AddressId]
+		
+		ORDER BY [Employee].[CompanyName];
