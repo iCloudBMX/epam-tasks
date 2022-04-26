74b4db2360969efa177730f120e97597f0262f79
Khondamir Abdukhoshimov <60962668+iCloudBMX@users.noreply.github.com>
2022-04-26 16:51:58 +0500

    Task4 Implemented

---
+++ Task1/SQLDB/Tirggers/Company_Insert_Trigger.sql
@@ -0,0 +1,12 @@
+﻿CREATE TRIGGER [dbo].[Company_Insert_Trigger]
+ON [dbo].[Employee]
+AFTER INSERT
+AS
+	DECLARE @CompanyName NVARCHAR(100)
+	DECLARE @AddressId INT
+	
+	SELECT @CompanyName = INSERTED.[CompanyName], @AddressId = INSERTED.[AddressId]
+		FROM INSERTED
+	
+	INSERT INTO [dbo].[Company]
+		([Name], [AddressId]) VALUES (@CompanyName, @AddressId)
